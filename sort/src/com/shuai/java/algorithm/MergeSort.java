449775666bd887831df7a623fc8d0e6feebe4cec
shuaizhimin <4402395>
2017-10-26 16:59:07 +0800

    n的阶乘

---
+++ sort/src/com/shuai/java/algorithm/MergeSort.java
@@ -0,0 +1,25 @@
+package com.shuai.java.algorithm;
+
+/**
+ * 作者: shuaizhimin
+ * 描述: 归并排序
+ * <p>
+ * 归并（Merge）排序法是将两个（或两个以上）有序表合并成一个新的有序表，
+ * 即把待排序序列分为若干个子序列，每个子序列是有序的。
+ * 然后再把有序子序列合并为整体有序序列
+ * <p>
+ * 日期: 2017-10-26
+ * 时间: 16:27
+ * 版本:
+ */
+public class MergeSort {
+    private static int[] a = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1};
+
+    public static void main(String args[]) {
+        mergeSort(a);
+    }
+
+    public static void mergeSort(int a[]) {
+
+    }
+}
